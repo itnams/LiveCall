f02c568ec3d905f192c384caaf30597a5592af41
nguyennam <itnams12@gmail.com>
2023-11-02 22:10:27 +0700

    Create a new project

---
+++ LiveCall/LiveCall/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using LiveCall.ViewModels;
+
+namespace LiveCall.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
